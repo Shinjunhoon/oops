5c87678b4b440e79b1b6c0f05d7889552c80a46f
신정훈 <sinjeonghun@sinjeonghun-ui-MacBookAir.local>
2025-11-14 19:55:23 +0900

    CORS 해결

--- src/main/java/com/example/oops/cofig/security/SecurityConfig.java
+++ src/main/java/com/example/oops/cofig/security/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig  {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000/","http://localhost:3000/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000/","http://localhost:3000/","https://oops.io.kr"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
